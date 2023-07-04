ebda36fa0849d39daf7351ed085a64e6560096f5
jenya-b <evgeny_rab@inbox.ru>
2023-07-04 20:57:14 +0300

    feat: add content

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,32 @@
 import { cardInfo } from './constants.js';
+
+const cardList = document.body.querySelector('.content__list');
+
+const createCard = (name, src, index) => {
+  const item = document.createElement('li');
+  item.classList.add('cards__item');
+  item.id = `item-${index}`;
+
+  const title = document.createElement('h2');
+  title.classList.add('cards__title');
+  title.textContent = name;
+
+  const imageWrap = document.createElement('div');
+  imageWrap.classList.add('cards__image');
+  const img = new Image();
+  img.src = src;
+  imageWrap.append(img);
+
+  item.append(imageWrap, title);
+
+  return item;
+};
+
+const addCardsOnPage = () => {
+  for (let i = 0; i < cardInfo.length; i++) {
+    const { title, url } = cardInfo[i];
+    cardList.append(createCard(title, url, i));
+  }
+};
+
+addCardsOnPage();
