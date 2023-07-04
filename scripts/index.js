9a4c0d12b22a4fad254dc71e617474b99e79cf7c
jenya-b <evgeny_rab@inbox.ru>
2023-07-04 21:04:21 +0300

    refactor: split the function

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,33 @@ import { cardInfo } from './constants.js';
 
 const cardList = document.body.querySelector('.content__list');
 
-const createCard = (name, src, index) => {
-  const item = document.createElement('li');
-  item.classList.add('cards__item');
-  item.id = `item-${index}`;
-
-  const title = document.createElement('h2');
-  title.classList.add('cards__title');
-  title.textContent = name;
-
+const createImage = (src) => {
   const imageWrap = document.createElement('div');
   imageWrap.classList.add('cards__image');
   const img = new Image();
   img.src = src;
   imageWrap.append(img);
 
-  item.append(imageWrap, title);
+  return imageWrap;
+};
+
+const createTitle = (name) => {
+  const title = document.createElement('h2');
+  title.classList.add('cards__title');
+  title.textContent = name;
+
+  return title;
+};
+
+const createCard = (name, src, index) => {
+  const item = document.createElement('li');
+  item.classList.add('cards__item');
+  item.id = `item-${index}`;
+
+  const title = createTitle(name);
+  const imageBlock = createImage(src);
+
+  item.append(imageBlock, title);
 
   return item;
 };
